c47b52516e0b4c84bcab47a65de74d04d66845df
Rian Douglas <rian.douglas@thetradedesk.com>
2022-03-09 12:13:21 -0800

    - Extract into class - Use a set of coordinates instead of 2d array

--- Program.cs
+++ Program.cs
@@ -1,57 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Solution
 {
-    public class Solution {
-
-        public enum Direction {
-            Up, Down, Left, Right
-        };
-
-        private static (int, int, Direction) GenerateArm(List<List<bool>> spiral, int x, int y, Direction dir, int len) {
-            if(len <= 0) return (x, y, dir);
-
-            switch(dir)
-            {
-                case Direction.Up:
-                    for(int i = 0; i < len; ++i) {
-                        spiral[y][x] = true;
-                        y--;
-                    }
-                    return (x, y+1, Direction.Right);
-                case Direction.Down:
-                    for(int i = 0; i < len; ++i) {
-                        spiral[y][x] = true;
-                        y++;
-                    }
-                    return (x, y-1, Direction.Left);
-                case Direction.Left:
-                    for(int i = 0; i < len; ++i) {
-                        spiral[y][x] = true;
-                        x--;
-                    }
-                    return (x+1, y, Direction.Up);
-                case Direction.Right:
-                    for(int i = 0; i < len; ++i) {
-                        spiral[y][x] = true;
-                        x++;
-                    }
-                    return (x-1, y, Direction.Down);
-            }
-            return (x, y, dir);
-        }
-
-        private static List<List<bool>> GenerateSpiral(int n)
+    public enum Direction {
+        Up, Down, Left, Right
+    };
+    public class Spiral
+    {
+        private static Dictionary<Direction, (int, int)> Steps = new Dictionary<Direction, (int, int)>()
         {
-            var spiral = new List<List<bool>>();
-
-            for(int i = 0; i < n; ++i) {
-                spiral.Add(new List<bool>());
-                for(int j = 0; j < n; ++j) {
-                    spiral[i].Add(false);
-                }
-            }
+            {Direction.Up, (0, -1)},
+            {Direction.Down, (0, 1)},
+            {Direction.Left, (-1, 0)},
+            {Direction.Right, (1, 0)},
+        };
+        private static Dictionary<Direction, Direction> NextDirection = new Dictionary<Direction, Direction>()
+        {
+            { Direction.Up, Direction.Right },
+            { Direction.Down, Direction.Left },
+            { Direction.Left, Direction.Up },
+            { Direction.Right, Direction.Down },
+        };
+        private HashSet<(int, int)> data;
+        public int Size { get; private set; }
+        
+        public bool IsSet(int x, int y) {
+            return data.Contains((x, y));
+        }
+        
+        public Spiral(int size)
+        {
+            this.Size = size;
+            this.data = GenerateSpiral(Size);
+        }
+        
+        private static HashSet<(int, int)> GenerateSpiral(int n)
+        {
+            var spiral = new HashSet<(int, int)>();
 
             var x = 0;
             var y = 0;
@@ -62,22 +49,42 @@ namespace Solution
 
             return spiral;
         }
-
-        public static void PrintSpiral(List<List<bool>> spiral) {
-            foreach(var row in spiral) {
-                foreach(var item in row) {
-                    Console.Write(item ? "#" : " ");
-                }
-                Console.WriteLine();
+        private static (int, int, Direction) GenerateArm(HashSet<(int, int)> spiral, int x, int y, Direction dir, int len) {
+            if(len <= 0) return (x, y, dir);
+            
+            var (xstep, ystep) = Steps[dir];
+            for(int i = 0; i < len; ++i)
+            {
+                spiral.Add((x, y));
+                x += xstep;
+                y += ystep;
             }
+            return (x - xstep, y - ystep, NextDirection[dir]);
         }
+        
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for(var y = 0; y < Size; ++y) {
+                for(var x = 0; x < Size; ++x) {
+                    if(data.Contains( (x, y) ))
+                        sb.Append('#');
+                    else
+                    sb.Append(' ');
+                }
+                sb.Append(System.Environment.NewLine);
+            }
+            return sb.ToString();
+        }
+    }
+    public class Solution {
+
 
         public static void Main(string[] args) {
 
-            for(int i = 1; i <= 7; ++i) {
-                var spiral = GenerateSpiral(i);
-                PrintSpiral(spiral);
-                Console.WriteLine();
+            for(int i = 1; i <= 10; ++i) {
+                var spiral = new Spiral(i);
+                Console.WriteLine(spiral);
                 Console.WriteLine();
             }
         }
